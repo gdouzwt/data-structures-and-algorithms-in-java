6ce1587d749fd193cd4421e4b564e171371be774
招文桃 <gdouzwt@gmail.com>
2020-08-07 22:20:54 +0800

    update list

---
+++ ch03/src/main/java/DoublyLinkedList.java
@@ -0,0 +1,51 @@
+/**
+ * A basic doubly linked list implementation.
+ */
+public class DoublyLinkedList<E> {
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node(E e, Node<E> p, Node<E> n) {
+            element = e;
+            prev = p;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setPrev(Node<E> p) {
+            prev = p;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
+    }  // end of nested Node class
+
+    // instance variables of the DoublyLinkedList
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    /**
+     * Constructs a new empty list.
+     */
+    public DoublyLinkedList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+}
---
+++ ch03/src/main/java/SinglyLinkedList.java
@@ -0,0 +1,85 @@
+public class SinglyLinkedList<E> {
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        public Node(E e, Node<E> n) {
+            element = e;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
+    }
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public SinglyLinkedList() {
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
+        return head.getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) {
+            return null;
+        }
+        return tail.getElement();
+    }
+
+    // update methods
+    public void addFirst(E e) {
+        head = new Node<>(e, head);
+        if (size == 0) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void addLast(E e) {
+        Node<E> newest = new Node<>(e, null);
+        if (isEmpty()) {
+            head = newest;
+        } else {
+            tail.setNext(newest);
+        }
+        tail = newest;
+        size++;
+    }
+
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        E answer = head.getElement();
+        head = head.getNext();
+        size--;
+        if (size == 0) {
+            tail = null;
+        }
+        return answer;
+    }
+}
