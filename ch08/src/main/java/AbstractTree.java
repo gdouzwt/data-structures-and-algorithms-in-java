dd369a13c529873969394ba09edb8e475e7294ef
招文桃 <gdouzwt@gmail.com>
2020-08-11 16:55:17 +0800

    implement heightBad() for AbstractTree

--- ch08/src/main/java/AbstractTree.java
+++ ch08/src/main/java/AbstractTree.java
@@ -1,5 +1,6 @@
-
-
+/**
+ * An abstract base class providing some functionality of the Tree interface.
+ */
 public abstract class AbstractTree<E> implements Tree<E> {
 
     @Override
@@ -22,4 +23,25 @@ public abstract class AbstractTree<E> implements Tree<E> {
         return size() == 0;
     }
 
+    /**
+     * @param p the position
+     * @return the number of levels separating Position p from the root.
+     */
+    public int depth(Position<E> p) {
+        if (isRoot(p))
+            return 0;
+        else
+            return 1 + depth(parent(p));
+    }
+
+    /**
+     * @return Returns the height of the tree.
+     */
+    private int heightBad() {  // works, but quadratic worst-case time.  可以用，但是最坏情况是平方阶
+        int h = 0;
+        for (Position<E> p : positions())
+            if (isExternal(p))  // only consider leaf positions
+                h = Math.max(h, depth(p));
+        return h;
+    }
 }
