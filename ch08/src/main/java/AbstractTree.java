d5d8aac154221d53a819178a286f2b5407f3de8d
招文桃 <gdouzwt@gmail.com>
2020-08-11 16:48:43 +0800

    add AbstractTree base class.

---
+++ ch08/src/main/java/AbstractTree.java
@@ -0,0 +1,25 @@
+
+
+public abstract class AbstractTree<E> implements Tree<E> {
+
+    @Override
+    public boolean isInternal(Position<E> p) throws IllegalArgumentException {
+        return numChildren(p) > 0;
+    }
+
+    @Override
+    public boolean isExternal(Position<E> p) throws IllegalArgumentException {
+        return numChildren(p) == 0;
+    }
+
+    @Override
+    public boolean isRoot(Position<E> p) throws IllegalArgumentException {
+        return p == root();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+}
